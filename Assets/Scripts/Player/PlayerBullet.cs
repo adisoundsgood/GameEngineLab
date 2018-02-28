6f073604e3b3d01f0b936f60e93e086df3cf59fd
Aditya Iyer <hungryadii@gmail.com>
2018-02-28 13:26:51 -0800

    hail mary

---
+++ Assets/Scripts/Player/PlayerBullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() {
+		rb.MovePosition(rb.position + transform.up * speed * Time.deltaTime);
+	}
+}
